52e0e792bfcc636aeb2f324e4f769d80a5e28464
Ckmena <mena.kthy@gmail.com>
2020-03-31 20:01:14 -0400

    pwa settings

---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "/Users/cindymena/Documents/fanshawe/w20/practicalApp/cindy-minisite",
+  "globPatterns": [
+    "**/*.{docx,html,json,css,png,xml,ico,svg,webmanifest,ai,php,js}"
+  ],
+  "swDest": "/Users/cindymena/Documents/fanshawe/w20/practicalApp/cindy-minisite/sw.js"
+};
